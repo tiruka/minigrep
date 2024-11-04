dfba47585e2cb879bd171494a071e28cfd8868aa
tiruka <tiruka.engineer@gmail.com>
2024-11-04 15:23:57 +0900

    add lib.rs to separate main logic

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,10 @@
 use std::env;
-use std::error::Error;
-use std::fs::File;
-use std::io::prelude::*;
 use std::process;
+use minigrep;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config = Config::new(args).unwrap_or_else(|err| {
+    let config = minigrep::Config::new(args).unwrap_or_else(|err| {
         println!("Problem when parsing arguments: {}", err);
         process::exit(1);
     });
@@ -14,32 +12,8 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
     
-    if let Err(e) = run(config) {
+    if let Err(e) = minigrep::run(config) {
         println!("Application error: {}", e);
         process::exit(1);
-    }
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>>{
-    let mut f = File::open(config.filename)?;
-    let mut contents = String::new();
-    f.read_to_string(&mut contents).expect("something is wrong");
-    println!("With text:\n{}", contents);
-    Ok(())
-}
-
-struct Config {
-    query: String,
-    filename: String,
-}
-
-impl Config {
-    fn new(args: Vec<String>) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-        let query = args[1].clone();
-        let filename = args[2].clone();
-        Ok(Config{query, filename})
     }
 }
