84797fe9f0b59c2c6b4f994d2de51a77d70e12bb
tiruka <tiruka.engineer@gmail.com>
2024-11-05 09:28:13 +0900

    modify to use stderr

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,11 @@ use minigrep;
 fn main() {
     let args: Vec<String> = env::args().collect();
     let config = minigrep::Config::new(args).unwrap_or_else(|err| {
-        println!("Problem when parsing arguments: {}", err);
+        eprintln!("Problem when parsing arguments: {}", err);
         process::exit(1);
     });
     if let Err(e) = minigrep::run(config) {
-        println!("Application error: {}", e);
+        eprintln!("Application error: {}", e);
         process::exit(1);
     }
 }
