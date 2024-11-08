877d18cbf0432743dc8b1a05cc654638eff89c85
tiruka <tiruka.engineer@gmail.com>
2024-11-08 11:59:30 +0900

    iterを使うように修正

--- src/lib.rs
+++ src/lib.rs
@@ -43,24 +43,11 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>>{
 }
 
 fn search<'a> (query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = vec![];
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
-    results
+    contents.lines().filter(|line| line.contains(query)).collect()
 }
 
 fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let query = query.to_lowercase();
-    let mut results = vec![];
-    for line in contents.lines() {
-        if line.to_lowercase().contains(&query) {
-            results.push(line);
-        }
-    }
-    results
+    contents.lines().filter(|line| line.to_lowercase().contains(query)).collect()
 }
 
 #[cfg(test)]
